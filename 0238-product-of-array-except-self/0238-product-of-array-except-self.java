986cca8b1281df0bb32b73fa12ae695ea7c5ac0c
Vaishnavi-github <162404138+JonnagaddalaVaishnavi@users.noreply.github.com>
2026-08-21 10:45:24 +0530

    Time: 2 ms (93.71%), Space: 71.8 MB (60.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,33 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // Arrays to store cumulative products from left and right sides
-        int[] left = new int[nums.length];
-        int[] right = new int[nums.length];
+        int n = nums.length;
+        int[] result = new int[n];
 
-        // Step 1: Build the "left product" array
-        // left[i] = product of all elements before index i
-        left[0] = 1; // nothing before the first element
-        for (int i = 1; i < nums.length; i++) {
-            left[i] = left[i - 1] * nums[i - 1];
-            // Example: left[2] = nums[0] * nums[1]
+        // Step 1: Build prefix products (left side)
+        // result[i] will store product of all elements before index i
+        result[0] = 1; // nothing before first element
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
         }
 
-        // Step 2: Build the "right product" array
-        // right[i] = product of all elements after index i
-        right[nums.length - 1] = 1; // nothing after the last element
-        for (int j = nums.length - 2; j >= 0; j--) {
-            right[j] = right[j + 1] * nums[j + 1];
-            // Example: right[2] = nums[3] * nums[4] * ... till end
+        // Step 2: Multiply with suffix products (right side)
+        // Use a single variable to track product of elements after index i
+        int rightProduct = 1; // nothing after last element
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * rightProduct;
+            rightProduct = rightProduct * nums[i];
         }
 
-        // Step 3: Final result
-        // For each index i, result[i] = product of all elements except nums[i]
-        // This is done by multiplying left[i] * right[i]
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = left[i] * right[i];
-        }
-
-        // Return the modified nums array as the result
-        return nums;
+        return result;
     }
 }
