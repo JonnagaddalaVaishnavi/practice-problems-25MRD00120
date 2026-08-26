43c105d974091c531618431adf9a42ab758b87b2
Vaishnavi-github <162404138+JonnagaddalaVaishnavi@users.noreply.github.com>
2026-08-26 18:31:58 +0530

    Time: 2 ms (99.31%), Space: 44.4 MB (54.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,33 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = plainString(s.toLowerCase());
-        int i = 0, j = str.length() - 1;
-        while(i<j){
-            if(str.charAt(i) != str.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
 
-    public static String plainString(String a){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<a.length(); i++){
-            char ch = a.charAt(i);
-            if((ch>='a' && ch<='z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')){
-                sb.append(ch);
+        // Initialize two pointers:
+        // i starts from the beginning, j starts from the end of the string
+        int i = 0, j = s.length() - 1;
+
+        // Continue until the two pointers meet or cross
+        while (i <= j) {
+            // Skip non-alphanumeric characters from the left side
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            } 
+            // Skip non-alphanumeric characters from the right side
+            else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            } 
+            else {
+                // Compare characters in a case-insensitive manner
+                if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                    // If mismatch found, it's not a palindrome
+                    return false;
+                }
+                // Move both pointers inward
+                i++;
+                j--;
             }
         }
-        return sb.toString();
+
+        // If all valid characters matched, the string is a palindrome
+        return true;
     }
 }
