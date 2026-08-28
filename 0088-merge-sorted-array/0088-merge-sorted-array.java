03bd0a52af00c7e6a1e8757febbef6c7a35b9ccf
Vaishnavi-github <162404138+JonnagaddalaVaishnavi@users.noreply.github.com>
2026-08-28 20:54:30 +0530

    Time: 0 ms (100%), Space: 44 MB (30.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,30 +1,41 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] res = new int[m+n];
-        int i = 0, j = 0, index=0;
-        while(i != m && j != n){
-            if(nums1[i] <= nums2[j]){
+        // Create a temporary result array to hold merged elements
+        int[] res = new int[m + n];
+
+        // i → pointer for nums1
+        // j → pointer for nums2
+        // index → pointer for res (merged array)
+        int i = 0, j = 0, index = 0;
+
+        // Merge elements while both arrays still have elements left
+        while (i != m && j != n) {
+            if (nums1[i] <= nums2[j]) {
                 res[index] = nums1[i];
                 i++;
-            }
-            else {
+            } else {
                 res[index] = nums2[j];
                 j++;
             }
             index++;
         }
-        while(i<m){
+
+        // Copy remaining elements from nums1 (if any)
+        while (i < m) {
             res[index] = nums1[i];
             index++;
             i++;
         }
 
-        while(j<n){
+        // Copy remaining elements from nums2 (if any)
+        while (j < n) {
             res[index] = nums2[j];
             index++;
             j++;
         }
-        for(int x = 0 ; x < res.length ; x++){
+
+        // Copy merged result back into nums1
+        for (int x = 0; x < res.length; x++) {
             nums1[x] = res[x];
         }
     }
