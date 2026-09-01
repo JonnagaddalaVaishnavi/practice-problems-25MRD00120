c24343e08f168e40afe3548ba9fec10e06fc6b84
Vaishnavi-github <162404138+JonnagaddalaVaishnavi@users.noreply.github.com>
2026-09-01 21:25:02 +0530

    Time: 1 ms (100%), Space: 47.1 MB (76.06%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,20 +1,34 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        // Result array to store squared values in sorted order
         int[] res = new int[nums.length];
-        int index = nums.length -1;
+
+        // 'index' points to the last position in result array
+        int index = nums.length - 1;
+
+        // Two pointers:
+        // i → start of array
+        // j → end of array
         int i = 0, j = nums.length - 1;
-        while(i <= j){
+
+        // Process until both pointers cross
+        while (i <= j) {
+            // Square values at both ends
             int leftSquare = nums[i] * nums[i];
             int rightSquare = nums[j] * nums[j];
-            if(leftSquare < rightSquare){
+
+            // Place the larger square at the current 'index' position
+            if (leftSquare < rightSquare) {
                 res[index] = rightSquare;
-                j--;
+                j--; // move right pointer left
             } else {
                 res[index] = leftSquare;
-                i++;
+                i++; // move left pointer right
             }
-            index--;
+            index--; // move result index backward
         }
+
+        // Return the sorted squares array
         return res;
     }
 }
